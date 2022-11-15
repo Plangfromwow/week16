e7f864e444e3457b4ae002994db4278e087119fe
Jacob Garcia <jacobgarcia@rocketmortgage.com>
2022-11-15 08:58:39 -0500

    adding instruments back end

---
+++ Instruments/Instruments/Controllers/InstrumentController.cs
@@ -0,0 +1,52 @@
+﻿using Instruments.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Instruments.Controllers
+{
+    [Route("instrument")]
+    [ApiController]
+    public class InstrumentController : ControllerBase
+    {
+        // Here are the routes: 
+        // GET /instrument/ - returns the list of instruments
+        // POST /instrument - adds new instrument to the database 
+        // DELETE /instrument/{id} - Deletes instrument by ID 
+        // POST /instrument/update -- updates the instrument that's passed in
+        // 
+
+        // as a review:
+        // with API Controllers, the name of the function is not part of the URL path
+        // so we can call the method whatever we want. 
+
+        //Read the database
+        [HttpGet]
+        public List<Instrument> ReadAll()
+        {
+            return Instrument.ReadAll();
+        }
+
+        //Create
+        [HttpPost]
+        public Instrument Add(Instrument inst)
+        {
+            return Instrument.Add(inst);
+        }
+        // Delete
+        [HttpDelete]
+        public string Delete(int id)
+        {
+            Instrument.Delete(id);
+            return "Ok";
+        }
+        // A lot of people use POST for their updates. 
+        // In fact, lots of devs use POST for EVERYTHING except the Get All that returns the whole world
+        [HttpPost("update")]
+        public string Update(Instrument inst)
+        {
+            Instrument.Update(inst);
+            return "It worked";
+        }
+
+    }
+}
---
+++ Instruments/Instruments/Models/Instrument.cs
@@ -0,0 +1,49 @@
+﻿using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+
+namespace Instruments.Models
+{
+    [Table("instrument")]
+    public class Instrument
+    {
+        [Key]
+        public int id { get; set; }
+        public string theType { get; set; }
+        public string color { get; set; }
+        public int year { get; set; }
+
+
+
+        // adding static properties and methods. 
+        // this is where we add our CRUD operations. 
+
+        public static MySqlConnection DB;
+
+        //read all rows from the table 
+
+        public static List<Instrument> ReadAll()
+        {
+            return DB.GetAll<Instrument>().ToList();
+        }
+
+        // create new istrument
+        public static Instrument Add(Instrument inst)
+        {
+            DB.Insert(inst);
+            return inst;
+        }
+
+        public static void Delete(int id)
+        {
+            Instrument inst = new Instrument() { id = id };
+            DB.Delete(inst);
+        }
+
+
+        //update the data
+        public static void Update(Instrument inst)
+        {
+            DB.Update(inst);
+        }
+    }
+}
